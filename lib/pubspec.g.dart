e7e4b9900614fc87e6e2442d8ad0a5a6145ab22e
Paul Barbenheim <paul@barbenheim.de>
2025-10-23 21:49:09 +0200

    feat: add go_router, riverpod and pubspec_extract dependency

---
+++ lib/pubspec.g.dart
@@ -0,0 +1,65 @@
+// This file is generated automatically, do not modify
+
+// ignore_for_file: public_member_api_docs, constant_identifier_names, avoid_classes_with_only_static_members
+
+mixin Pubspec {
+  static final buildDate = DateTime.utc(2025, 10, 23, 19, 35, 28);
+
+  static const name = 'asu';
+
+  static const description = 'A new Flutter project.';
+
+  static const publish_to = 'none';
+
+  static const versionFull = '1.0.0+1';
+
+  static const version = '1.0.0';
+
+  static const versionSmall = '1.0';
+
+  static const versionMajor = 1;
+
+  static const versionMinor = 0;
+
+  static const versionPatch = 0;
+
+  static const versionBuild = 1;
+
+  static const versionPreRelease = '';
+
+  static const versionIsPreRelease = false;
+
+  static const environment = <dynamic, dynamic>{
+    'sdk': '^3.9.2',
+  };
+
+  static const dependencies = <dynamic, dynamic>{
+    'flutter': <dynamic, dynamic>{
+      'sdk': 'flutter',
+    },
+    'cupertino_icons': '^1.0.8',
+    'flutter_riverpod': '^3.0.3',
+    'riverpod_annotation': '^3.0.3',
+    'go_router': '^16.3.0',
+  };
+
+  static const dev_dependencies = <dynamic, dynamic>{
+    'flutter_test': <dynamic, dynamic>{
+      'sdk': 'flutter',
+    },
+    'flutter_lints': '^5.0.0',
+    'riverpod_generator': '^3.0.3',
+    'build_runner': '^2.7.1',
+    'custom_lint': '^0.8.0',
+    'riverpod_lint': '^3.0.3',
+    'pubspec_extract': '^2.1.1',
+  };
+
+  static const flutter = <dynamic, dynamic>{
+    'uses-material-design': true,
+  };
+
+  static const pubspec_extract = <dynamic, dynamic>{
+    'destination': 'lib/pubspec.g.dart',
+  };
+}
